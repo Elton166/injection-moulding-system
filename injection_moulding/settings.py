28a88569a6925b62b5981c9ed987c4b3990f0225
Elton166 <eltonoct@gmail.com>
2026-04-28 00:26:11 +0200

    Fix database config - use SQLite by default, PostgreSQL if available

--- injection_moulding/settings.py
+++ injection_moulding/settings.py
@@ -80,12 +80,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'injection_moulding.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Use PostgreSQL if DATABASE_URL is provided
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
